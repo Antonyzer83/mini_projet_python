5b2d19d2d78b333b318e3fd9ffbf143b8027dde7
Antony CASTANER <antony.castanerpynov.com>
2019-04-24 11:55:55 +0200

    Add graphique games

--- graphique/cerclescolores.py
+++ graphique/cerclescolores.py
@@ -1,9 +1,59 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from tkinter import *
+from errors import *
+
 
 class CerclesColores:
-    coordonnees = None
+    taille = None
+    couleur = None
+    couleurs = ['black', 'blue', 'green', 'red']
+    x1 = 10
+    y1 = 190
+    x2 = 190
+    y2 = 10
 
     def __init__(self):
-        print('\nJeu Cercles Colorés')
+        print('\nJeu Télécran')
+        try:
+            taille = int(input('Entrer la taille : '))
+            couleur = input('Entrer la couleur : ')
+            if self.verifierTaille(taille) and self.verifierCouleur(couleur):
+                self.taille = taille
+                self.couleur = couleur
+                self.creationFenetre()
+            else:
+                BadVerification()
+        except:
+            NoInt()
+
+    def verifierTaille(self, taille):
+        if 1 <= taille <= 9:
+            return True
+        else:
+            return False
+
+    def verifierCouleur(self, couleur):
+        if couleur in self.couleurs:
+            return True
+        else:
+            return False
+
+    def creationFenetre(self):
+        self.fen1 = Tk()
+        self.fen1.title('Cercles Colorés')
+        self.can = Canvas(self.fen1, bg='dark grey', height=200, width=200)
+        self.can.pack(side=LEFT)
+        self.bou2 = Button(self.fen1, text='Tracer une ligne', command=self.dessiner())
+        self.bou2.pack()
+        self.fen1.mainloop()
+
+    def dessiner(self):
+        for i in range(20):
+            self.can.create_line(self.x1, self.y1, self.x2, self.y2, width=2, fill=self.couleur)
+
+            self.y2 += 10
+            self.y1 -= 10
+
+
--- graphique/telecran.py
+++ graphique/telecran.py
@@ -1,35 +1,69 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from errors import *
+from tkinter import *
 
 
 class Telecran:
-    taille = None
-    couleur = None
-    couleurs = ['rouge', 'vert', 'bleu', 'orange', 'violet', 'rose', 'blanc', 'noir']
+    x1 = 250
+    y1 = 250
+    x2 = 250
+    y2 = 250
 
     def __init__(self):
         print('\nJeu Télécran')
-        try:
-            taille = int(input('\nEntrer la taille : '))
-            couleur = input('Entrer la couleur : ')
-            if self.verifierTaille(taille) and self.verifierCouleur(couleur):
-                self.taille = taille
-                self.couleur = couleur
-            else:
-                BadVerification()
-        except:
-            NoInt()
+        self.fen = Tk()
+        self.fen.title('Télécran')
+        self.can = Canvas(self.fen, bg='cyan', height=500, width=500)
+        self.can.pack(side=LEFT)
+        self.but_left = Button(self.fen, text='Gauche', command=self.deplacerGauche)
+        self.but_left.pack(ipadx=10, ipady=5, padx=10, pady=10)
+        self.but_right = Button(self.fen, text='Right', command=self.deplacerDroite)
+        self.but_right.pack(ipadx=10, ipady=5, padx=10, pady=10)
+        self.but_top = Button(self.fen, text='Haut', command=self.deplacerHaut)
+        self.but_top.pack(ipadx=10, ipady=5, padx=10, pady=10)
+        self.but_bottom = Button(self.fen, text='Bas', command=self.deplacerBas)
+        self.but_bottom.pack(ipadx=10, ipady=5, padx=10, pady=10)
+        self.but_delete = Button(self.fen, text='Reset', command=self.viderCanvas)
+        self.but_delete.pack(ipadx=10, ipady=5, padx=10, pady=10)
+        self.fen.mainloop()
 
-    def verifierTaille(self, taille):
-        if taille < 10:
-            return True
-        else:
-            return False
+    def deplacerGauche(self):
+        self.x1 = self.x2
+        self.y1 = self.y2
+        self.x2 -= 10
+        self.dessinerLigne()
+        pass
 
-    def verifierCouleur(self, couleur):
-        if couleur in self.couleurs:
-            return True
-        else:
-            return False
+    def deplacerDroite(self):
+        self.x1 = self.x2
+        self.y1 = self.y2
+        self.x2 += 10
+        self.dessinerLigne()
+        pass
+
+    def deplacerHaut(self):
+        self.x1 = self.x2
+        self.y1 = self.y2
+        self.y2 -= 10
+        self.dessinerLigne()
+        pass
+
+    def deplacerBas(self):
+        self.x1 = self.x2
+        self.y1 = self.y2
+        self.y2 += 10
+        self.dessinerLigne()
+        pass
+
+    def dessinerLigne(self):
+        self.can.create_line(self.x1, self.y1, self.x2, self.y2, width=2)
+        pass
+
+    def viderCanvas(self):
+        self.can.delete("all")
+        self.x1 = 250
+        self.y1 = 250
+        self.x2 = 250
+        self.y2 = 250
+        pass
